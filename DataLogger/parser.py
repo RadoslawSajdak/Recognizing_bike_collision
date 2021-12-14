dc476aa7bec20a220373948dbeb127487caa91ae
RadoslawSajdak <sajdak.radoslaw@gmail.com>
2021-12-14 23:13:33 +0100

    parser as loop, added filtered images

--- DataLogger/parser.py
+++ DataLogger/parser.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import butter, lfilter, freqz
 
-file_rd = open("raw_accel13_12_2021_073423.txt", "r")
+file_rd = open("raw_accel13_12_2021_072106.txt", "r")
 
 lines = file_rd.readlines()
 
@@ -51,41 +51,16 @@ for line in lines:
 filtered_data = []
 titles = ["Gyroscope X", "Gyroscope Y", "Gyroscope Z", "Accelerometer X", "Accelerometer Y", "Accelerometer Z"]
 y_labels = ["dps", "dps", "dps", "g[m/s^2]", "g[m/s^2]", "g[m/s^2]"]
+x_labels = ["t[s]", "t[s]", "t[s]", "t[s]", "t[s]", "t[s]"] 
 
-for axis, n, tit, y_l in zip(raw_data, range(len(raw_data)), titles, y_labels):
+for axis, n, tit, y_l, x_l in zip(raw_data[3:], range(len(raw_data[3:])), titles[3:], y_labels[3:], x_labels[3:]):
     filtered_data.append(butter_lowpass_filter(axis[CUT_FRONT:], cutoff, fs, order))
     t = np.arange(len(axis[CUT_FRONT:]))/416
-    plt.subplot(2,3,n + 1)
+    plt.subplot(3,1,n + 1)
     plt.plot(t, filtered_data[n])
     plt.ylabel(y_l)
+    plt.xlabel(x_l)
+    plt.tight_layout()
     plt.title(tit)
-
-
-
-"""
-plt.subplot(2,3,1)
-plt.plot(t,xg[CUT_FRONT:])
-plt.ylabel("dps")
-plt.title("Gyroscope X")
-plt.subplot(2,3,2)
-plt.plot(t,yg[CUT_FRONT:])
-plt.ylabel("dps")
-plt.title("Gyroscope Y")
-plt.subplot(2,3,3)
-plt.plot(t,zg[CUT_FRONT:])
-plt.title("Gyroscope Z")
-plt.ylabel("dps")
-plt.subplot(2,3,4)
-plt.plot(t,xa[CUT_FRONT:])
-plt.ylabel("g[m/s^2]")
-plt.title("Accelerometer X")
-plt.subplot(2,3,5)
-plt.plot(t,ya[CUT_FRONT:])
-plt.ylabel("g[m/s^2]")
-plt.title("Accelerometer Y")
-plt.subplot(2,3,6)
-plt.plot(t,za[CUT_FRONT:])
-plt.ylabel("g[m/s^2]")
-plt.title("Accelerometer Z")
-"""
+    
 plt.show()
